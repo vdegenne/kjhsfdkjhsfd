e228fe3926b8f5b0f2a3d4b16ea22a23302ec468
Valentin Degenne <vdegenne@gmail.com>
2023-05-01 19:31:08 +0000

    ...

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+
+// Try completing LitElement to see if VSCode suggests the import
+// It should work in the project once you've manually import somewhere
+// But why does it not work *initially* ?
+
+class TestElement extends LitE
